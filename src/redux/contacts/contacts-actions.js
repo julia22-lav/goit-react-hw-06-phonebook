402785c2d9bd24ee54fc7bad2748edbbb3629303
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-05-02 15:37:31 +0200

    added contacts-actions.js

---
+++ src/redux/contacts/contacts-actions.js
@@ -0,0 +1,25 @@
+import types from './contacts-actions';
+import { v4 as genId } from 'uuid';
+
+const addContact = ({ name, number, id }) => {
+  return {
+    type: types.AddContact,
+    payload: { name, number, id: genId() },
+  };
+};
+
+const deleteContact = contactId => {
+  return {
+    type: types.DeleteContact,
+    payload: contactId,
+  };
+};
+
+const filterChange = value => {
+  return {
+    type: types.FilterChange,
+    payload: value,
+  };
+};
+
+export default { addContact, deleteContact, filterChange };
