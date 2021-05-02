22382249e6ee11083e5fb3946e76153b3366efc4
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-05-02 15:13:41 +0200

    refactored Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import { v4 as genId } from 'uuid';
 import PropTypes from 'prop-types';
 import s from './Filter.module.css';
+import { connect } from 'react-redux';
+import actions from '../../redux/contacts/contacts-actions';
 
 class Filter extends Component {
   render() {
@@ -29,4 +31,12 @@ Filter.propTypes = {
   filterChange: PropTypes.func,
 };
 
-export default Filter;
+const mapStateToProps = state => ({
+  filterState: state.contacts.filter,
+});
+
+const mapDispatchToProps = dispatch => ({
+  filterChange: event => dispatch(actions.filterChange(event.target.value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
